a56d646b30eab5a8d527be631a1566e862b3576e
shelling-out <rami-zidan@hotmail.com>
2023-06-01 17:31:27 +0300

    init Ballon

---
+++ js/Ballon.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three' ; 
+
+const loadingManager = new THREE.LoadingManager() ;
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+const WallTexture = textureLoader.load('wall.jpg');
+
+
+class Ballon {
+    constructor( scene , Width , MassOfBallon , HeightOfBallon , 
+        RadiusOfBallon , TempratureInsideBallon , PercentageOfFire 
+        , WindVelocity ,Color  ){
+        this.scene = scene ;
+        this.Width = Width ;  
+        this.MassOfBallon =  MassOfBallon ; 
+        this.HeightOfBallon = HeightOfBallon ; // HeightOfBallon = HeightFromSea + distance from center of ballon to center of earth
+        this.RadiusOfBallon = RadiusOfBallon ;
+        this.TempratureInsideBallon = TempratureInsideBallon;
+        this.PercentageOfFire = PercentageOfFire ;
+        this.WindVelocity = WindVelocity  ;
+        this.Color = Color ;
+        const FullBallon = new THREE.Group();
+        this.FullBallon = FullBallon ;
+        this.Draw() ; 
+        console.log('wtf');
+    }
+    DrawCabin(){
+
+        const geometry = new THREE.BoxGeometry(this.Width , this.Width  , this.Width , this.Width  , this.Width , this.Width , this.Width) ; 
+        const material = new THREE.MeshBasicMaterial({ color: this.Color , map: WallTexture }); 
+        const box = new THREE.Mesh(geometry , material ) ;
+        this.FullBallon.add(box) ;
+        
+    }
+    DrawBallon(){
+        const points = [];
+        for ( let i = 0; i < 18; i++ ) {
+            points.push( new THREE.Vector2(  Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+        }
+        const geometry = new THREE.LatheGeometry( points  );
+        const material = new THREE.MeshBasicMaterial({ color: this.Color , map: WallTexture }); 
+        const ballon = new THREE.Mesh(geometry , material ) ;
+        ballon.rotation.x = Math.PI ;
+        ballon.position.y = 30 ;
+        this.FullBallon.add(ballon) ;
+    }
+    Draw(){
+        this.DrawCabin();
+        this.DrawBallon();
+        this.scene.add(this.FullBallon) ;
+    }
+    AnimateBallon(cnt){
+        
+    }
+}
+
+
+export default Ballon ;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import * as THREE from 'three';
 
+import Ballon from './Ballon'; // importing Ballon
+import {MapControls} from 'three/examples/jsm/controls/MapControls' // import the camera controller
+import * as dat from 'dat.gui'
+
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
 
@@ -7,17 +13,42 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
-
-
+// creating ballon and drawing it.
+const ballon = new Ballon(scene , 3 , 3  , 3, 3 ,  3 , 3 , 3 , 'white');
+ballon.Draw() ;
 
 camera.position.z = 5;
-const geometry = new THREE.BoxGeometry( 2, 2,2 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+// init dat.gui 
+const gui = new dat.GUI({ width:600});
+
+
+let ConfigOptions ={ 
+	Mass:1 , 
+	Raduis:3,
+	Fire:0,
+	WindVeloctiy:0
+};
+
+gui.add(ConfigOptions , 'Mass' ).min(1).max(200).step(5) ;
+gui.add(ConfigOptions , 'Raduis') ;
+gui.add(ConfigOptions ,  'Fire').min(0).max(100).step(5) ;
+gui.add(ConfigOptions , 'WindVeloctiy' ) ;
+
+
+// init MapControls
+const controls = new MapControls(camera , renderer.domElement) ;
+
+
+
+let cnt = 0 ;
+
 function animate()
 {
+	// update controls
+	controls.update() ;
+	
+	
  	requestAnimationFrame( animate );
  	renderer.render( scene, camera );
 	cube.rotation.x += 0.01;
